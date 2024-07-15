d39790aecc8fb63a8db1a1e8bff50160fc83fc5b
root <root@DESKTOP-RHA5CJR.localdomain>
2024-07-14 22:17:46 -0700

    submit functions 2 lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "party"){
+    console.log (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(txt = 'go to the office'){
+
+}
+
+function wrapAdjective(strng = "%&%&"){
+    function innerwrapAdjective(adj = "special"){
+        return `you are ${adj}`;
+    }
+}
